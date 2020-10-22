85de6fb1466e75d785b526760d48d5650d50d6d5
ravigavde <52853915+ravigavde@users.noreply.github.com>
2020-10-22 21:05:15 +0530

    Add files via upload

---
+++ Day 1 and 2/script/dist/index.dev.js
@@ -0,0 +1,48 @@
+/*
+let aa = [1,2,3,4,5,6,7,8,10];
+aa.forEach(function(element){
+    console.log(element);
+});
+
+for(let a = 0; a<aa.length;a++)
+{
+    console.log(aa[a]);
+}
+
+
+let person = {fname:"John", lname:"Doe", age:25};
+
+let text = "";
+let x;
+for (x in person) {
+    console.log("the " + x + " is "+person[x]  );
+}
+
+
+var cars = ['BMW', 'Volvo', 'Mini'];
+var x;
+
+for (x of cars) {
+    document.write(x + "<br >");
+}
+
+*/
+// math
+// Min max 
+// round up 
+// pi 
+// date
+// let today = new Date(2019,1);
+// console.log(today);
+// document.write(today)
+// let person = {name:"Ravi", roll : 12 , class : "Msc cA"};
+// console.log(person.name);
+// let a = [1,2,3,4,5,6,6,7];
+// a.forEach(function(element) {
+//     console.log(element);
+// });
+// for(key in person)
+// {
+//     console.log("Key is "+key +" value is "+person[key]);
+// }
+"use strict";
